50c275ad3b2cc0aceda3930670ba440c132059a4
Robert Weber <robert.weber@logrhythm.com>
2017-11-10 15:31:26 -0700

    AddBlock for bolt

--- block/chain.go
+++ block/chain.go
@@ -1,23 +1,94 @@
 package block
 
+import (
+	"github.com/boltdb/bolt"
+	log "github.com/cihub/seelog"
+)
+
+const blocksBucket = "myblocks"
+
 //Chain of blocks (simple)
 type Chain struct {
-	blocks []*Block
-	pow    POWBuilder
+	Tip []byte
+	pow POWBuilder
+	db  *bolt.DB
+}
+
+func (c *Chain) Close() error {
+	return c.db.Close()
 }
 
 //AddBlock to chain
-func (c *Chain) AddBlock(data []byte) {
-	if c.blocks == nil {
-		c.blocks = []*Block{}
-		c.blocks = append(c.blocks, NewGenesisBlock(c.pow))
-	}
-	prev := c.blocks[len(c.blocks)-1]
-	new := NewBlock(c.pow, data, prev.GetHash())
-	c.blocks = append(c.blocks, new)
+func (c *Chain) AddBlock(data []byte) (err error) {
+	var lastHash []byte
+
+	err = c.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		lastHash = b.Get([]byte("l"))
+
+		return nil
+	})
+
+	newBlock := NewBlock(c.pow, data, lastHash)
+
+	err = c.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		j, err := newBlock.Serialize()
+		if err != nil {
+			return err
+		}
+		err = b.Put(newBlock.Headers.Hash, j)
+		if err != nil {
+			return err
+		}
+		err = b.Put([]byte("l"), newBlock.Headers.Hash)
+		if err != nil {
+			return err
+		}
+		c.Tip = newBlock.Headers.Hash
+		return nil
+	})
+	return err
 }
 
 //NewBlockChain with valid genesis
-func NewBlockChain(pow POWBuilder) *Chain {
-	return &Chain{blocks: []*Block{NewGenesisBlock(pow)}, pow: pow}
+func NewBlockChain(pow POWBuilder, dbfile string) *Chain {
+	var tip []byte
+	db, err := bolt.Open(dbfile, 0600, nil) // leaving this open???
+	if err != nil {
+		log.Error("cannot create chain: ", err)
+		return nil
+	}
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+
+		if b == nil {
+			gen := NewGenesisBlock(pow)
+			b, err := tx.CreateBucket([]byte(blocksBucket))
+			if err != nil {
+				return err
+			}
+			j, err := gen.Serialize()
+			if err != nil {
+				return err
+			}
+			err = b.Put(gen.Headers.Hash, j)
+			if err != nil {
+				return err
+			}
+			err = b.Put([]byte("l"), gen.Headers.Hash)
+			tip = gen.Headers.Hash
+			return nil
+		}
+
+		tip = b.Get([]byte("l"))
+		return nil
+	})
+	if err != nil {
+		db.Close()
+		log.Error("cannot update chain: ", err)
+		return nil
+	}
+
+	return &Chain{Tip: tip, db: db, pow: pow}
 }
--- block/chain_test.go
+++ block/chain_test.go
@@ -2,34 +2,58 @@ package block
 
 import (
 	"testing"
+	"os"
+	"io/ioutil"
 
 	log "github.com/cihub/seelog"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestChains(t *testing.T) {
+
 	defer log.Flush()
 	Convey("Build A Chain", t, func() {
+		tmpfile, err := ioutil.TempFile("/tmp", "testchains")
+		if err != nil {
+			t.FailNow()
+		}
+		defer os.Remove(tmpfile)
 		p := NewSha256Pow(2)
 
-		c := NewBlockChain(p)
+		c := NewBlockChain(p, tmpfile)
+		So(c, ShouldNotBeNil)
+		defer c.Close()
 		c.AddBlock([]byte("hello world"))
 		So(len(c.blocks), ShouldEqual, 2)
 		So(c.blocks[1].Data, ShouldResemble, []byte("hello world"))
 	})
 
 	Convey("Constructor", t, func() {
+		tmpfile, err := ioutil.TempFile("/tmp", "testchains")
+		if err != nil {
+			t.FailNow()
+		}
+		defer os.Remove(tmpfile)
 		p := NewSha256Pow(2)
 
-		c := NewBlockChain(p)
+		c := NewBlockChain(p, tmpfile)
+		So(c, ShouldNotBeNil)
+		defer c.Close()
 		So(c.blocks, ShouldNotBeNil)
 		So(c.blocks, ShouldNotBeEmpty)
 	})
 
 	Convey("add some stuff", t, func() {
+		tmpfile, err := ioutil.TempFile("/tmp", "testchains")
+		if err != nil {
+			t.FailNow()
+		}
+		defer os.Remove(tmpfile)
 		p := NewSha256Pow(2)
 
 		c := NewBlockChain(p)
+		So(c, ShouldNotBeNil)
+		defer c.Close()
 		So(c.blocks, ShouldNotBeNil)
 		So(c.blocks, ShouldNotBeEmpty)
 		c.AddBlock([]byte("hello"))
